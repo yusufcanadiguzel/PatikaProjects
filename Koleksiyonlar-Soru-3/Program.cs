89a4c3a66c4f40e254d6a527f61792903ad6c0ff
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2023-10-10 17:07:35 +0300

    Koleksiyonlar-Soru-3 - Done
    İstenenler doğrultusunda ilgili algoritma geliştirilmiştir.

---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+
+char[] vowels = { 'a', 'e', 'ı', 'i', 'u', 'ü', 'o', 'ö' };
+List<char> textVowels = new List<char>();
+
+string text;
+
+Console.Write("Bir cümle giriniz: ");
+text = (Console.ReadLine()).ToLower();
+
+foreach (var character in text)
+{
+    if(!textVowels.Contains(character) && vowels.Contains(character))
+        textVowels.Add(character);
+}
+
+Console.Write("Girilen metindeki sesli harfler:");
+textVowels.ForEach(x => Console.Write($" {x}"));
